b22e7083bd91e829cd83d24a97c5baee13b131e4
Harald <harald921@hotmail.com>
2017-04-01 00:44:33 +0200

    Separate input code from movement code
    This makes it possible to make AI characters in a clean and powerful way. It also makes it much cleaner to add support for many different controllers.

---
+++ Equinexus/Assets/Code/Character.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public struct Input
+    {
+        public Quaternion targetRotation;
+        public Vector3 moveDirection;
+
+        public bool isShooting;
+        public bool isThrowingWeapon;
+        public bool isAiming;
+
+        public Input(Quaternion inTargetRotation, Vector3 inMoveDirection, bool inIsShooting, bool inIsThrowingWeapon, bool inIsAiming)
+        {
+            targetRotation      = inTargetRotation;
+            moveDirection       = inMoveDirection;
+            isShooting          = inIsShooting;
+            isThrowingWeapon    = inIsThrowingWeapon;
+            isAiming            = inIsAiming;
+        }
+    }
+    public Input input;
+
+    [System.Serializable]
+    public struct Stats
+    {
+        public float health;
+        public float moveSpeed;
+        public float turnSpeed;
+        public float throwStrength;
+    }
+    [SerializeField] Stats _stats;
+
+    [SerializeField] GameObject _weaponsHolder;
+
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    void HandleInput()
+    {
+        // Movement
+        {
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.AddForce(input.moveDirection.normalized * _stats.moveSpeed);
+        }
+
+        // Aiming
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, input.targetRotation, _stats.turnSpeed * Time.deltaTime);
+        }
+
+        // Shooting
+        {
+            if (input.isShooting)
+                for (int i = 0; i < _weaponsHolder.transform.childCount; i++)
+                    _weaponsHolder.transform.GetChild(i).gameObject.GetComponent<Weapon>().TryShoot();
+        }
+
+        // Throwing weapons 
+        {
+            if (input.isThrowingWeapon)
+                for (int i = 0; i < _weaponsHolder.transform.childCount; i++)
+                {
+                    GameObject currentWeaponGO = _weaponsHolder.transform.GetChild(i).gameObject;
+                    currentWeaponGO.transform.SetParent(transform.parent);
+                    currentWeaponGO.GetComponent<Rigidbody>().AddForce(transform.forward * _stats.throwStrength);
+                }
+        }
+    }
+}
---
+++ Equinexus/Assets/Code/PlayerInput.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    Character _targetCharacter;
+
+
+    void Start()
+    {
+        _targetCharacter = GetComponent<Character>();
+    }
+
+    void Update()
+    {
+        Character.Input input = new Character.Input(Quaternion.identity, Vector3.zero, false, false, false);
+
+        // Get shooting/aim input
+        if (Input.GetMouseButton(0)) input.isShooting   = true;
+        if (Input.GetMouseButton(1)) input.isAiming     = true;
+
+        // Get movement input
+        if (Input.GetKey(KeyCode.W))      input.moveDirection += Vector3.forward;
+        else if (Input.GetKey(KeyCode.S)) input.moveDirection += Vector3.back;
+        if (Input.GetKey(KeyCode.A))      input.moveDirection += Vector3.left;
+        else if (Input.GetKey(KeyCode.D)) input.moveDirection += Vector3.right;
+
+        // Calculate aim target
+        Vector3 aimTarget = UnityEngine.Input.mousePosition;
+        aimTarget.z = Mathf.Abs(Camera.main.transform.position.y - transform.position.y);
+        aimTarget = Camera.main.ScreenToWorldPoint(aimTarget);
+        aimTarget = new Vector3(aimTarget.x, 1, aimTarget.z);
+        input.targetRotation = Quaternion.LookRotation(aimTarget - transform.position);
+
+        _targetCharacter.input = input;
+    }
+}
