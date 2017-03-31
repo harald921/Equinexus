c8ce0fa734818292b7cb75397f8698302d53f18d
Harald <harald921@hotmail.com>
2017-03-31 21:37:10 +0200

    Try to repair repo 2

---
+++ Equinexus/Assets/Code/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    GameObject _playerGO;
+    Vector3 _posOffset;
+
+    void Start()
+    {
+        _playerGO = GameObject.Find("Player");
+        _posOffset = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = _playerGO.transform.position + _posOffset;
+    }
+}
