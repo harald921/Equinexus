8224f97b5369570bcdbddb5c72637cfd76ced71d
Unknown <harald921@hotmail.com>
2017-05-15 01:28:53 +0200

    Implement super basic pathfinding

---
+++ Equinexus/Assets/Code/AIRusher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIRusher : MonoBehaviour 
+{
+    /* Fields */
+    public Transform _target;
+    NavMeshAgent _navMeshAgent;
+
+	/* Base */
+    void Start()
+    {
+        _target = GameObject.Find("Player").transform;
+
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        _navMeshAgent.destination = _target.position;
+    }
+
+    private void Update()
+    {
+        _navMeshAgent.destination = _target.position;
+    }
+
+    /* External */
+
+    /* Internal */
+}
+
+
