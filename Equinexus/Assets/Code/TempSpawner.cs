00fd209b252b69ba2606c2b37e0b5fa30c3fe42f
Unknown <harald921@hotmail.com>
2017-05-22 19:49:47 +0200

    Add basic enemy spawner

---
+++ Equinexus/Assets/Code/TempSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempSpawner : MonoBehaviour 
+{
+    /* Fields */
+    [SerializeField] GameObject _prefabToSpawn;
+    [SerializeField] float _spawnInterval = 10.0f;
+
+    float _spawnTimer;
+
+    /* Base */
+    private void Start()
+    {
+        _spawnTimer = Random.Range(1.0f, _spawnInterval);
+    }
+
+    private void Update()
+    {
+        _spawnTimer += Time.deltaTime;
+
+        if (_spawnTimer >= _spawnInterval)
+        {
+            Instantiate(_prefabToSpawn, transform.position, Quaternion.identity);
+            _spawnTimer = 0.0f;
+        }
+    }
+
+    /* External */
+
+    /* Internal */
+}
